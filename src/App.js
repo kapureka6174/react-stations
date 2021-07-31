f659ac5a2b23800bfb4ba4dace72c71cb50139ea
kapureka6174 <mitsu.n.0109@gmail.com>
2021-07-31 12:52:56 +0900

    ✨ Add site_description and dog_image

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ export const App = () => {
   return (
     <>
       <header>Dogアプリ</header>
+      <p>犬の画像を表示するサイトです</p>
+      <img
+        src="https://images.dog.ceo/breeds/boxer/n02108089_3258.jpg"
+        alt="犬の画像"
+      />
     </>
   )
 }
