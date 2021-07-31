18cbedfb6d86e2c2875010df36c26e6af8a4ee3a
kapureka6174 <mitsu.n.0109@gmail.com>
2021-07-31 18:42:31 +0900

    Add useState feature

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@
 
 import * as React from 'react'
 import './App.css'
+import { useState } from 'react'
 
 /**
  *
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/boxer/n02108089_3258.jpg',
+  )
   return (
     <>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです</p>
-      <img
-        src="https://images.dog.ceo/breeds/boxer/n02108089_3258.jpg"
-        alt="犬の画像"
-      />
+      <img src={dogUrl} alt="犬の画像" />
     </>
   )
 }
